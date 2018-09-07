18c03ebc1906bbfbfb8490f8ab34870964bacccb
Kovács Máté <kismate90@gmail.com>
2018-09-07 14:33:14 +0200

    Added getters and setters for Transaction.

--- src/main/java/com/supercharge/banking/model/Transaction.java
+++ src/main/java/com/supercharge/banking/model/Transaction.java
@@ -25,4 +25,52 @@ public class Transaction {
 
     public Transaction() {
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public User getTarget() {
+        return target;
+    }
+
+    public void setTarget(User target) {
+        this.target = target;
+    }
 }
