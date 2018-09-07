ec3e10f195dba2878a84561a5a0f410db59893f1
Kovács Máté <kismate90@gmail.com>
2018-09-07 15:10:29 +0200

    Added Repository methods to find a user's transaction history.

--- src/main/java/com/supercharge/banking/Repository/TransactionRepository.java
+++ src/main/java/com/supercharge/banking/Repository/TransactionRepository.java
@@ -1,7 +1,12 @@
 package com.supercharge.banking.Repository;
 
 import com.supercharge.banking.model.Transaction;
+import com.supercharge.banking.model.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAllByUserIdOrderByDateDesc(Long id);
+    List<Transaction> findAllByUserIdAndTypeOrderByDateDesc(Long id, TransactionType type);
 }
