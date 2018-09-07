b9fd1595fa14eae31b5167fae1dbc501fadd1d34
Kovács Máté <kismate90@gmail.com>
2018-09-07 14:55:36 +0200

    Created User Service class.

---
+++ src/main/java/com/supercharge/banking/service/UserService.java
@@ -0,0 +1,17 @@
+package com.supercharge.banking.service;
+
+import com.supercharge.banking.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    final
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
